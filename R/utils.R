539dbe75c00852ab9d13c34ebfcdfd5af525bdd7
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-11-07 21:37:25 -0500

    make checks

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,25 @@ check_proportions <- function(p0, p1) {
                      Relative risk is not defined."))
   }
 }
+
+check_count_inputs <- function(x0, x1, n0, n1) {
+  if (any(x0 < 0 | x1 < 0)) {
+    cli::cli_abort(c("x" = "{.var x0} and {.var x1} must be non-negative."))
+  }
+  if (any(n0 <= 0 | n1 <= 0)) {
+    cli::cli_abort(c("x" = "{.var n0} and {.var n1} must be positive."))
+  }
+  if (any(x0 > n0)) {
+    cli::cli_abort(c("x" = "{.var x0} must not exceed {.var n0}."))
+  }
+  if (any(x1 > n1)) {
+    cli::cli_abort(c("x" = "{.var x1} must not exceed {.var n1}."))
+  }
+}
+
+check_confidence_level <- function(level) {
+  if (any(level <= 0 | level >= 1)) {
+    cli::cli_abort(c("x" = "{.var level} must be between 0 and 1."))
+  }
+}
+
